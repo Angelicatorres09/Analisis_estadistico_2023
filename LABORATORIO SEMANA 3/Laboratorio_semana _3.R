5c13b9e3962443974ba657452bda51d5af447df0
AnGeLiCa Torres <angelica.torres.garcia09gmail.com>
2023-02-22 19:43:37 -0600

    LABORATORIO SEMANA 3 COMPLETO

--- LABORATORIO SEMANA 3/Laboratorio_semana _3.R
+++ LABORATORIO SEMANA 3/Laboratorio_semana _3.R
@@ -1,6 +1,6 @@
 #Angelica Torres García
 #22/02/2023
-#
+#2173388
 
 
 # LABORATORIO SEMANA 3
@@ -21,7 +21,115 @@ head(prof_url_2)
 
 #Datos de URL seguras (https): Dropbox y Github
 
+library(repmis) 
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+head(conjunto)
+
 library(readr)
+File <- paste0("https://raw.githubusercontent.com/mgtagle/", 
+               "202_Analisis_Estadistico_2020/master/cuadro1.csv")
+inventario <- read_csv(File)
+
+head(inventario)
+
+
+# #PARTE 2. OPERACIONES CON LA BASE DE DATOS ------------------------------
+
+#Media 
+#el signo de $ informa que necesitamos la calumna dbh
+
+mean(trees$dbh) 
+
+#Desviación estandar
+
+sd(trees$dbh)
+
+#Selección mediante restricciones
+
+#Los condicionantes restrictivas más empleadas son:
+
+#igual o mayor (>=), mayor que (>), igual que (==)
+#igual o menor (<=), menor que (<), no igual (!=)
+
+# Indica la sumatoria de los individuos en el objeto tree con un dbh < a 10
+
+sum(trees$dbh < 10)
+
+#para saber cuales son los individuos que son inferiores al diámetro (dbh< 10 cm) se usa la función which
+
+which(trees$dbh < 10)
+
+#Excluir los diámetros que se encuentran en la parcela 2. no tenemos esos datos en la tabla
+
+trees.13 <- trees[!(trees$parcela=="2"),]
+trees.13 
+
+#Selección de una submuestra
+
+#se puede obtener mediante la funcion subset
+
+trees.1 <- subset(trees, dbh <= 10)
+head(trees.1)
+
+mean(trees$dbh)
+
+mean(trees.1$dbh)
+
+hist(trees$dbh, ylab = "Frecuencia", xlab = "DBH", main = "Muestra orinal trees")
+
+hist(trees.1$dbh, ylab = "Frecuencia", xlab = "DBH", main = "dbh < 10 cm. trees.1")
+
+
+# PARTE 3. REPRESENTACIÓN GRAFICA -----------------------------------------
+
+#HISTOGRAMAS
+
+mamiferos <- read.csv("https://www.openintro.org/data/csv/mammals.csv")
+head(mamiferos)
+
+# Se trabajara con la variable total_sleep para generar el histograma:
+
+hist(mamiferos$total_sleep)
+
+#HISTOGRAMA PRESENTABLE
+
+hist(mamiferos$total_sleep)
+
+
+hist(mamiferos$total_sleep, #datos
+     
+     xlim = c(0,20), ylim = c(0,14), main = "Total de horas sueño de las 39 especies", #cambiar titulo
+     xlab = "Horas sueño", #cambiar eje de las x
+     ylab = "Frecuencia", #cambiar eje de las y
+     las = 1, #cambiar orientacion de y
+     col = "pink") #cambiar color a las barras
+
+#Barplot o gráfico de barras
+
+data("chickwts")
+head(chickwts[c(1:2,42:43, 62:64), ])
+
+#Primeramente tendremos que acomodar los datos en columnas
+
+feeds <- table(chickwts$feed)
+feeds
+
+barplot(feeds)
+
+# para ordenar de forma decreciente
+
+barplot(feeds[order(feeds, decreasing = TRUE)])
+
+#Barplot presentable
+
+barplot(feeds[order(feeds, decreasing = FALSE)], main ="frecuencia por tipo de alimentación",
+        xlab = "Numero de pollos",
+        las=1,
+        col= "orange",
+        horiz = TRUE)
+
+        
+        
 
 
 
